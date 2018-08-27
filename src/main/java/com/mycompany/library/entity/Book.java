752c8bd6e50120d3158a30443df0c6d4b083aead
sumeet89 <40126288+sumeet89@users.noreply.github.com>
2018-08-27 22:03:20 +0200

    Create Book.java

---
+++ src/main/java/com/mycompany/library/entity/Book.java
@@ -0,0 +1,105 @@
+package com.mycompany.library.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "T_BOOK")
+public class Book implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Column(name = "AUTHOR", nullable = false)
+    private String author;
+
+    @Column(name = "PUBLISHED_YEAR", nullable = false)
+    private Short publishedYear;
+
+    @Column(name = "PRICE", nullable = false)
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Short getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(Short publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book)obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publishedYear=" + publishedYear + ", price=" + price + "]";
+    }
+
+}
